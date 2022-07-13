afd183833983f80f02fc705b681b02bb4ce63fc8
khan <omeryagci3295@gmail.com>
2022-07-13 14:07:44 +0300

    navbar change

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import classes from "../Navbar/navbar.module.scss";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   // const [isLoggin, setIsloggin] = useState(false);
@@ -19,7 +19,11 @@ const Navbar = () => {
             <NavLink to="/about">About</NavLink>
           </li>
           <li>
-            <a target="_blank" href="https://github.com/omer-yagci">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/omer-yagci"
+            >
               Github
             </a>
           </li>
